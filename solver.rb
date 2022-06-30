db985b098a3011121fe43568b68189f87f735fa3
zkr024 <zkr.bm.02403@gmail.com>
2022-06-30 15:49:16 -0500

    Update fizzbuzz method
    What to return when the number is not divisible by 3 or 5

--- solver.rb
+++ solver.rb
@@ -18,5 +18,7 @@ class Solver
     return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
     return 'fizz' if (number % 3).zero?
     return 'buzz' if (number % 5).zero?
+
+    number.to_s
   end
 end
