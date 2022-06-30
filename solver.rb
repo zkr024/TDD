76d196357972fc82830e78c3c277cfa15b22d2c9
zkr024 <zkr.bm.02403@gmail.com>
2022-06-30 14:45:24 -0500

    test to check the parameter

--- solver.rb
+++ solver.rb
@@ -1,2 +1,3 @@
 class Solver
+  def factorial(number); end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,5 +9,13 @@ describe Solver do
     it 'Returns a new Solver object' do
       expect(@solver).to be_an_instance_of Solver
     end
+
+    it 'Only acepts integers >= 0' do
+      expect { @solver.factorial(-5) }.to raise_error(RuntimeError)
+    end
+
+    it 'Only acepts integers' do
+      expect { @solver.factorial('f') }.to raise_error(RuntimeError)
+    end
   end
 end
